f576c7627e6629ceb7b41c9701708b4b576386d2
Bruno Marée <bruno.maree@student.hepl.be>
2025-06-01 16:18:49 +0200

    add improvements

--- wp-content/themes/portfolio/header.php
+++ wp-content/themes/portfolio/header.php
@@ -3,16 +3,16 @@
 <head>
 	<meta name="description" content="<?php bloginfo('description'); ?>">
 	<meta charset="<?php bloginfo('charset'); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0, minimum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title><?php bloginfo('name');
         wp_title($sep = '·') ?></title>
 	<link rel="stylesheet" href="<?= portfolio_asset('css/main.css'); ?>">
-	<script src="<?= portfolio_asset('js/main.js') ?>"></script>
+	<script defer src="<?= portfolio_asset('js/main.js') ?>"></script>
     <?php wp_head(); ?>
 
 </head>
-<body <?php add_data_page_attribute(); ?> class="body">
+<body class="body">
 <?php wp_body_open(); ?>
 <header class="header">
 	<h1 class="hidden"><?php bloginfo('name'); ?></h1>
